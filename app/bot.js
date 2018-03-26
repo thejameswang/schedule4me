3d9854c81eb94b9fe62e146ff9ac6d95d1c852d1
katslump <kat.slump@gmail.com>
2018-03-26 15:11:33 -0700

    Checking for user responses.

--- app/bot.js
+++ app/bot.js
@@ -11,31 +11,36 @@ export default function bot() {
 
     // bot.run();
 
-    bot.on('start', function() {
-        // more information about additional params https://api.slack.com/methods/chat.postMessage
-            var params = {
-                icon_emoji: ':cat:'
-            };
-
-            // define channel, where bot exist. You can adjust it there https://my.slack.com/services
-            bot.postMessageToChannel('general', 'meow!', params);
-
-            // define existing username instead of 'user_name'
-            bot.postMessageToUser('user_name', 'meow!', params);
-
-            // If you add a 'slackbot' property,
-            // you will post to another user's slackbot channel instead of a direct message
-            bot.postMessageToUser('user_name', 'meow!', { 'slackbot': true, icon_emoji: ':cat:' });
-
-            // define private group instead of 'private_group', where bot exist
-            bot.postMessageToGroup('private_group', 'meow!', params);
-    });
+    // bot.on('start', function() {
+    //     // more information about additional params https://api.slack.com/methods/chat.postMessage
+    //         var params = {
+    //             icon_emoji: ':cat:'
+    //         };
+    //
+    //         // define channel, where bot exist. You can adjust it there https://my.slack.com/services
+    //         bot.postMessageToChannel('general', 'meow!', params);
+    //
+    //         // define existing username instead of 'user_name'
+    //         bot.postMessageToUser('user_name', 'meow!', params);
+    //
+    //         // If you add a 'slackbot' property,
+    //         // you will post to another user's slackbot channel instead of a direct message
+    //         bot.postMessageToUser('user_name', 'meow!', { 'slackbot': true, icon_emoji: ':cat:' });
+    //
+    //         // define private group instead of 'private_group', where bot exist
+    //         bot.postMessageToGroup('private_group', 'meow!', params);
+    // });
 
     /**
      * @param {object} data
      */
     bot.on('message', function(data) {
         // all ingoing events https://api.slack.com/rtm
-        console.log(data);
+
+        if (typeof(data.text) !== "undefined") {
+            console.log(data.text);
+        }
+
     });
+
 }

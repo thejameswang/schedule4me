3064c5da7c019c9b6989690fb4713d3fed9e2a52
James Wang <j.wang5441@gmail.com>
2018-03-26 18:23:19 -0700

    fixes issues

--- app/trainer.js
+++ app/trainer.js
@@ -1,8 +1,18 @@
 var apiai = require('apiai');
-
-var app = apiai(process.env.APIAI_TOKEN);
+const bodyParser = require('body-parser')
+// var app = apiai(process.env.APIAI_TOKEN);
 
 export default function trainer(app) {
+
+    app.post('/test', function(req, res) {
+      var response = req.body.result && req.body.result.parameters && req.body.result.parameters.scheduling ? req.body.result.parameters.echoText : 'There was an issue';
+      console.log(response)
+      return res.json({
+        response,
+        displayTest: response,
+        source: 'schedule4me'
+      })
+    })
 
     var request = app.textRequest('<Your text query>', {
         sessionId: '<unique session id>'

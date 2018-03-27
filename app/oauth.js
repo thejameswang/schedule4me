3e175f5d42cbc8620a4a5be2c8f758d323431923
katslump <kat.slump@gmail.com>
2018-03-26 17:06:02 -0700

    Removed file

--- app/oauth.js
+++ app/oauth.js
@@ -1,20 +1,20 @@
 import fs from 'fs';
 import google from 'googleapis';
-import googleAuth from 'google-auth-library';
 import readline from 'readline';
 import Calendar from '../models/Calendar';
-
+import googleAuth from 'google-auth-library';
 
 
 export default function oauth(app) {
 
     // If modifying these scopes, delete your previously saved credentials
     // at ~/.credentials/calendar-nodejs-quickstart.json
-    var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+    var SCOPES = ['https://www.googleapis.com/auth/calendar'];
     var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
         process.env.USERPROFILE) + '/.credentials/';
     var TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
-
+    console.log(process.env.HOME || process.env.HOMEPATH ||
+        process.env.USERPROFILE);
     // Load client secrets from a local file.
     fs.readFile('client_secret.json', function processClientSecrets(err, content) {
       if (err) {
@@ -40,7 +40,6 @@ export default function oauth(app) {
       var redirectUrl = credentials.installed.redirect_uris[0];
       var auth = new googleAuth();
       var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
-
       // Check if we have previously stored a token.
       fs.readFile(TOKEN_PATH, function(err, token) {
         if (err) {
@@ -52,14 +51,7 @@ export default function oauth(app) {
       });
     }
 
-    /**
-     * Get and store new token after prompting for user authorization, and then
-     * execute the given callback with the authorized OAuth2 client.
-     *
-     * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
-     * @param {getEventsCallback} callback The callback to call with the authorized
-     *     client.
-     */
+
     function getNewToken(oauth2Client, callback) {
       var authUrl = oauth2Client.generateAuthUrl({
         access_type: 'offline',
@@ -101,46 +93,6 @@ export default function oauth(app) {
       console.log('Token stored to ' + TOKEN_PATH);
     }
 
-    /**
-     * Lists the next 10 events on the user's primary calendar.
-     *
-     * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
-     */
-    function listEvents(auth) {
-      var calendar = google.calendar('v3');
-      calendar.events.list({
-        auth: auth,
-        calendarId: 'primary'
-    },{qs: {
-        timeMin: (new Date()).toISOString(),
-        maxResults: 10,
-        singleEvents: true,
-        orderBy: 'startTime'
-        }
-}, function(err, response) {
-        if (err) {
-          console.log('The API returned an error: ' + err);
-          return;
-        }
-        var events = response.items;
-        if (events.length == 0) {
-          console.log('No upcoming events found.');
-        } else {
-          console.log('Upcoming 10 events:');
-          for (var i = 0; i < events.length; i++) {
-            var event = events[i];
-            var start = event.start.dateTime || event.start.date;
-            console.log('%s - %s', start, event.summary);
-          }
-        }
-      });
-    }
-
-    /**
- * Lists the next 10 events on the user's primary calendar.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
 function listEvents(auth) {
   var calendar = google.calendar('v3');
   calendar.events.list({
@@ -155,7 +107,7 @@ function listEvents(auth) {
       console.log('The API returned an error: ' + err);
       return;
     }
-    var events = response.items;
+    var events = response.data.items;
     if (events.length == 0) {
       console.log('No upcoming events found.');
     } else {
@@ -182,7 +134,7 @@ function listEvents(auth) {
             'timeZone': 'America/Los_Angeles'
           },
           'end': {
-            'dateTime': '2018-03-28T17:00:00-07:00',
+            'dateTime': '2018-03-28T17:00:00-09:00',
             'timeZone': 'America/Los_Angeles'
           },
           'attendees': [

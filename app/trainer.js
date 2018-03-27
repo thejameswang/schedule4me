4a155bf7e6e8f2c97cd340c3f0f9194ab31963d8
James Wang <j.wang5441@gmail.com>
2018-03-27 11:47:36 -0700

    adds training

--- app/trainer.js
+++ app/trainer.js
@@ -5,13 +5,14 @@ var apiaiapp = apiai(process.env.APIAI_TOKEN);
 export default function trainer(event) {
     let sender = event.user;
     let message = event.text;
-
+    console.log(sender)
+    console.log(event.text)
     var request = apiaiapp.textRequest(message, {
         sessionId: 'schedule4me'
     });
 
     request.on('response', function(response) {
-        // console.log(response);
+        console.log(response);
         let aiText = response.result.fulfillment.speech;
         console.log(aiText)
     });

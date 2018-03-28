5c7ad3f3db7dfbae069cfb52bfae077230bb8128
James Wang <j.wang5441@gmail.com>
2018-03-28 13:26:33 -0700

    returns message to slack, and confirms reminder

--- app/trainer.js
+++ app/trainer.js
@@ -5,11 +5,8 @@ var apiaiapp = apiai(process.env.APIAI_TOKEN);
 export default function trainer(event) {
     let sender = event.user;
     let message = event.text;
-<<<<<<< HEAD
     console.log(sender, 'sending')
     console.log(event.text, 'text')
-=======
->>>>>>> 61edb0d3b81ffdcca53a2a4d3a3844f74f00cc49
 
 
     return new Promise(function(resolve, reject) {
@@ -18,16 +15,6 @@ export default function trainer(event) {
             sessionId: 'schedule4me'
         });
 
-<<<<<<< HEAD
-    request.on('response', function(response) {
-        // console.log(response, 'given response that comes back');
-        response.user = sender;
-        // let aiText = response.result.fulfillment.speech;
-        // console.log(aiText)
-        console.log(response)
-        return response
-    });
-=======
         request.on('response', function(response) {
             let newResponse = JSON.stringify(response);
             respondingWith = JSON.parse(newResponse);
@@ -38,9 +25,8 @@ export default function trainer(event) {
             console.log(error);
             reject(error);
         });
-        
+
         request.end();
->>>>>>> 61edb0d3b81ffdcca53a2a4d3a3844f74f00cc49
 
     });
 

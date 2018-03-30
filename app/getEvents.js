96589a8b5c8401246c0840794cf7eed0e49a6873
katslump <kat.slump@gmail.com>
2018-03-29 18:36:48 -0700

    Get events for next week

---
+++ app/getEvents.js
@@ -0,0 +1,39 @@
+import {google} from 'googleapis';
+
+export default function getEvents(oauth2Client) {
+    var calendar = google.calendar('v3');
+    let today = new Date();
+    let upcomingEvents = [];
+
+    calendar.events.list({
+        auth: oauth2Client,
+        calendarId: 'primary',
+        timeMin: (today).toISOString(),
+        timeMax: new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000),
+        maxResults: 50,
+        singleEvents: true,
+        orderBy: 'startTime'
+    }, function(err, response) {
+        if (err) {
+            console.log('The API returned an error: ' + err);
+            return;
+        }
+        var events = response.data.items;
+        if (events.length == 0) {
+            console.log('No upcoming events found.');
+        } else {
+            console.log('Upcoming events for the next week:');
+            for (var i = 0; i < events.length; i++) {
+                var event = events[i];
+                var start = event.start.dateTime || event.start.date;
+                upcomingEvents.push({
+                    time: start,
+                    event: event.summary
+                });
+                console.log('%s - %s', start, event.summary);
+            }
+        }
+    });
+    return upcomingEvents;
+
+}
